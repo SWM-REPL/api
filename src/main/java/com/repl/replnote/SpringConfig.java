c3d4d0a1348fd9b0b263c353b99c2e6a09acde22
Jinwoo Lee <binarywooo@gmail.com>
2023-06-01 22:59:33 +0900

    feat: Login 기능 구현

--- src/main/java/com/repl/replnote/SpringConfig.java
+++ src/main/java/com/repl/replnote/SpringConfig.java
@@ -2,6 +2,7 @@ package com.repl.replnote;
 
 import com.repl.replnote.user.repository.SpringDataJpaUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -14,4 +15,10 @@ public class SpringConfig {
     public SpringConfig(SpringDataJpaUserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    // setter 주입
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
